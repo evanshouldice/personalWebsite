b4fd847de78ea26ff9b698c6d5e75d1f1d2afd13
dicect <65144276+dicect@users.noreply.github.com>
2022-09-24 15:24:24 -0400

    contact me section template added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import "./contact.css"
+import {MdOutlineMail} from 'react-icons/md'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <MdOutlineMail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>eshouldice1443@conestogac.on.ca</h5>
+            <a href="mailto:eshouldice1443@conestogac.on.ca" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="text" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
