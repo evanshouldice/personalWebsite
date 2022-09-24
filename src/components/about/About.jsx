129db7679ad6fc5959e29edc78e5b6892e5e4b1d
dicect <65144276+dicect@users.noreply.github.com>
2022-09-23 20:17:09 -0400

    added floating nav bar and started about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import "./about.css"
-
+import ME from '../../assets/me.png'
+import{FaAward} from 'react-icons/fa'
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>8 Months Working</small>
+            </article>
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>8 Months Working</small>
+            </article>
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>8 Months Working</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
