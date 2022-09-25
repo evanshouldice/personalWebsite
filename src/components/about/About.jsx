6729ecc7324f82e223103c9f427fec47330f6293
dicect <65144276+dicect@users.noreply.github.com>
2022-09-25 13:44:50 -0400

    updated about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -20,27 +20,27 @@ const About = () => {
         <div className="about__content">
           <div className="about__cards">
             <article className='about__card'>
-              <FaAward className='about__icon'/>
-              <h5>Experience</h5>
-              <small>8 Months Working</small>
-            </article>
-            <article className='about__card'>
               <BiCodeAlt className='about__icon'/>
-              <h5>Current Stack</h5>
-              <small>Typescript, React, Node.js</small>
+              <h5>Current Occupation</h5>
+              <small>Full Stack Engineer Intern - Thinktum Inc (May 2022 - Present)</small>
             </article>
             <article className='about__card'>
               <MdSchool className='about__icon'/>
               <h5>Current Program</h5>
-              <small>Software Engineering Technology</small>
+              <small>Software Engineering Technology (Co-op) - Conestoga College (Completed 2 of 3 years)</small>
+            </article>
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Past Education</h5>
+              <small>Bachelor of Arts Degree - Wilfrid Laurier University (2010-2014)</small>
             </article>
           </div>
 
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Libero officia quibusdam vitae? Sint culpa dicta delectus 
-            sequi quidem deleniti perspiciatis vitae ipsa a eveniet ipsam, 
-            commodi et quasi nisi dolores.
+            This site is a work in progress and so am I.  I went back to school at the age of 29 to switch careers
+            and build things!  I am currently in the middle of my second 4-month co-op term.  I work remotely from Canmore, Alberta
+            for Thinktum Inc, an insurtech startup based in Waterloo, Ontario.  My current role is Full Stack Engineer Intern.
+            I looking for new opportunities and I am available for a 4 or 8-month co-op term starting in January 2023.
           </p>
 
           <a href='#contact' className='btn btn-primary'>Let's Talk</a>
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Resume from '../../assets/EvanShouldiceResume.pdf'
+import Resume from '../../assets/EvanShouldiceResume.png'
 
 const CTA = () => {
   return (
